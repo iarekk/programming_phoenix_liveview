f6227de52257d9990a4a5a4b4b9b00ad9d5e97c8
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2024-01-28 22:53:38 +0000

    make the game work, plus the reset functionality (exercise)

--- pento/lib/pento_web/live/wrong_live.ex
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -1,16 +1,57 @@
 defmodule PentoWeb.WrongLive do
+  require Logger
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
+    rando = Enum.random(1..10)
+
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       time: time(),
+       to_guess: rando,
+       game_won: false
+     )}
   end
 
   def handle_event("guess", %{"number" => number}, socket) do
-    score = socket.assigns.score - 1
+    to_guess = socket.assigns.to_guess
+
+    Logger.info("to_guess: #{inspect(to_guess)}, number: #{inspect(number)}")
+
+    score = socket.assigns.score
+    make_message(to_guess, number |> String.to_integer(), score, socket)
+  end
+
+  def handle_params(_unsigned_params, _uri, socket) do
+    rando = Enum.random(1..10)
+
+    # reset the params to the default set, same as what's in mount/3
+    # TODO obviously need to reduce the duplication
+    {:noreply,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       time: time(),
+       to_guess: rando,
+       game_won: false
+     )}
+  end
+
+  def make_message(to_guess, number, current_score, socket) when to_guess == number do
+    message = "You guessed #{number}. Correct!"
+
+    {:noreply,
+     assign(socket, message: message, score: current_score + 10, time: time(), game_won: true)}
+  end
+
+  def make_message(_, number, current_score, socket) do
+    # what's the difference between :reply and :noreply?
     message = "You guessed #{number}. It is wrong. Guess again."
 
-    # what's the difference between :reply and :noreply?
-    {:noreply, assign(socket, message: message, score: score, time: time())}
+    {:noreply,
+     assign(socket, message: message, score: current_score - 1, time: time(), game_won: false)}
   end
 
   def time(), do: DateTime.utc_now() |> to_string()
@@ -24,14 +65,24 @@ defmodule PentoWeb.WrongLive do
     </h2>
     <br />
     <h2>
-      <%= for n <- 1..10 do %>
+      <%= if(not @game_won) do %>
+        <%= for n <- 1..10 do %>
+          <.link
+            class="bg-blue-500 hover:bg-blue-700
+          text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
+            phx-click="guess"
+            phx-value-number={n}
+          >
+            <%= n %>
+          </.link>
+        <% end %>
+      <% else %>
         <.link
           class="bg-blue-500 hover:bg-blue-700
           text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
-          phx-click="guess"
-          phx-value-number={n}
+          patch={~p"/guess"}
         >
-          <%= n %>
+          Restart
         </.link>
       <% end %>
     </h2>
