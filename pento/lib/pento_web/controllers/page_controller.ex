3471fc6e3f36d293380d3255cd943b3d532e101c
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2024-01-28 19:20:32 +0000

    add the /lolz page to demonstrate routing

--- pento/lib/pento_web/controllers/page_controller.ex
+++ pento/lib/pento_web/controllers/page_controller.ex
@@ -6,4 +6,10 @@ defmodule PentoWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def lolz(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :lolz, layout: false)
+  end
 end
