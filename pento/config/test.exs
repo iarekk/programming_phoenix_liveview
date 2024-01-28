a096ed87f3549af13c7a0e6307f0e000257c396f
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2024-01-28 23:22:04 +0000

    update PG creds

--- pento/config/test.exs
+++ pento/config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :pento, Pento.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "ectotest",
+  password: "ectotest",
   hostname: "localhost",
   database: "pento_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
