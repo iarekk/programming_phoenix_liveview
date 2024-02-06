72c73a8bd9c1a0871256ef9b4dc78beef5eb95d5
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2024-02-06 18:54:04 +0000

    Ch2/add username field (#7)
    * generate migrations to create the users.username field and populate it with emails
    
    * username registration now works
    
    * fix several tests
    
    * render username in game, fix all tests
    
    * fix message casing

--- pento/lib/pento/accounts/user.ex
+++ pento/lib/pento/accounts/user.ex
@@ -4,6 +4,7 @@ defmodule Pento.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :username, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
@@ -36,9 +37,10 @@ defmodule Pento.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :username])
     |> validate_email(opts)
     |> validate_password(opts)
+    |> validate_username(opts)
   end
 
   defp validate_email(changeset, opts) do
@@ -58,6 +60,14 @@ defmodule Pento.Accounts.User do
     # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
     # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
     |> maybe_hash_password(opts)
+  end
+
+  defp validate_username(changeset, opts) do
+    changeset
+    |> validate_required([:username])
+    |> validate_format(:username, ~r/^[^\s]+$/, message: "must have no spaces")
+    |> validate_length(:username, max: 160)
+    |> maybe_validate_unique_username(opts)
   end
 
   defp maybe_hash_password(changeset, opts) do
@@ -82,6 +92,16 @@ defmodule Pento.Accounts.User do
       changeset
       |> unsafe_validate_unique(:email, Pento.Repo)
       |> unique_constraint(:email)
+    else
+      changeset
+    end
+  end
+
+  defp maybe_validate_unique_username(changeset, opts) do
+    if Keyword.get(opts, :validate_username, true) do
+      changeset
+      |> unsafe_validate_unique(:username, Pento.Repo)
+      |> unique_constraint(:username)
     else
       changeset
     end
