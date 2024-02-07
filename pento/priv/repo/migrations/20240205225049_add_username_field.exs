45db2b88d11dee0c2748ab95cdb998451b414b4c
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2024-02-07 22:02:26 +0000

    drop data migrations from the migration list

--- pento/priv/repo/migrations/20240205225049_add_username_field.exs
+++ pento/priv/repo/migrations/20240205225049_add_username_field.exs
@@ -1,9 +1,14 @@
 defmodule Pento.Repo.Migrations.AddUsernameField do
   use Ecto.Migration
 
+  # following advice in this thread, dropping the data migration from the migration list
+  # https://elixirforum.com/t/why-does-this-code-work-as-2-separate-migrations-but-not-as-a-single-one/61506/3
+
   def change do
     alter(table(:users)) do
-      add :username, :citext
+      add :username, :citext, null: false
     end
+
+    create(unique_index(:users, :username))
   end
 end
