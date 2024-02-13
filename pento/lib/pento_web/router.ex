c47b438239a53108134f4ca937aea0bb7652df75
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2024-02-13 17:33:25 +0000

    Create the FrequentlyAskedQuestions live view with required question and optional vote count and answer

--- pento/lib/pento_web/router.ex
+++ pento/lib/pento_web/router.ex
@@ -84,6 +84,13 @@ defmodule PentoWeb.Router do
       live "/products/:id", ProductLive.Show, :show
       live "/products/:id/show/edit", ProductLive.Show, :edit
       live "/products/:id/show/mark_down", ProductLive.Show, :mark_down
+
+      live "/questions", QuestionLive.Index, :index
+      live "/questions/new", QuestionLive.Index, :new
+      live "/questions/:id/edit", QuestionLive.Index, :edit
+
+      live "/questions/:id", QuestionLive.Show, :show
+      live "/questions/:id/show/edit", QuestionLive.Show, :edit
     end
   end
 
