0e7d675a9a40ade22ae1dd9f646ceb4e207429c1
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2024-06-04 14:31:53 +0100

    now supports validation of the input for the name and email

--- pento/lib/pento_web/live/promo_live.ex
+++ pento/lib/pento_web/live/promo_live.ex
@@ -1,9 +1,42 @@
 defmodule PentoWeb.PromoLive do
+  require Logger
   use PentoWeb, :live_view
   alias Pento.Promo
   alias Pento.Promo.Recipient
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign_recipient()
+     |> clear_form()}
+  end
+
+  def handle_event(
+        "validate",
+        %{"recipient" => recipient_params},
+        %{assigns: %{recipient: recipient}} = socket
+      ) do
+    changeset =
+      recipient |> Promo.change_recipient(recipient_params) |> Map.put(:action, :validate)
+
+    Logger.info("handle event recipient params: #{inspect(recipient_params)}")
+
+    {:noreply, assign_form(socket, changeset)}
+  end
+
+  def assign_recipient(socket) do
+    socket |> assign(:recipient, %Recipient{})
+  end
+
+  def clear_form(socket) do
+    socket
+    |> assign_form(
+      socket.assigns.recipient
+      |> Promo.change_recipient()
+    )
+  end
+
+  def assign_form(socket, changeset) do
+    socket |> assign(:form, to_form(changeset))
   end
 end
